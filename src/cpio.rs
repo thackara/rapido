32fbaf5e1c315aa8b1bc6a970d7a7d5be907476b
David Disseldorp <ddiss@suse.de>
2025-10-27 09:44:57 +0100

    cpio: accept metadata via new ArchiveMd type
    The fs::Metadata type is private, which makes it difficult to create
    cpio archive entries for items which don't exist locally. Create a
    new public ArchiveMd type for this purpose, which can easily be
    instantiated from fs::Metadata .
    This also has the benefit of moving some of the duplicate 64-bit ->
    32-bit (cpio) overflow checks into a single function.
    There's no backward compatibility offered, so this change will break
    existing callers.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- src/cpio.rs
+++ src/cpio.rs
@@ -2,11 +2,11 @@
 // Copyright (C) 2021 SUSE LLC
 
 use std::convert::TryInto;
+use std::convert::TryFrom;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 use std::os::unix::ffi::OsStrExt;
-use std::os::unix::fs::FileTypeExt;
 use std::os::unix::fs::MetadataExt as UnixMetadataExt;
 use std::path::Path;
 
@@ -32,6 +32,16 @@ macro_rules! dout {
 
 pub const NEWC_HDR_LEN: u64 = 110;
 pub const PATH_MAX: u64 = 4096;
+
+// format: octal posix mode bits
+pub const S_IFIFO: u32 = 0o010000;
+pub const S_IFCHR: u32 = 0o020000;
+pub const S_IFDIR: u32 = 0o040000;
+pub const S_IFBLK: u32 = 0o060000;
+pub const S_IFREG: u32 = 0o100000;
+pub const S_IFLNK: u32 = 0o120000;
+pub const S_IFSOCK: u32 = 0o140000;
+pub const S_IFMT: u32 = 0o170000;
 
 pub struct ArchiveProperties {
     // first inode number to use. @ArchiveState.ino increments from this.
@@ -88,18 +98,109 @@ impl ArchiveState<'_> {
     }
 }
 
+// fs::Metadata is private. This allows callers to explicitly set md
+pub struct ArchiveMd {
+    // ino increments for each cpio entry from props.initial_ino
+    // nlink is hardcoded 1 for files, retained for dirs
+    pub nlink: u32,
+    pub mode: u32,
+    // may be overridden by props.fixed_uid/gid/mtime
+    pub uid: u32,
+    pub gid: u32,
+    pub mtime: u32,
+    // major and minor hardcoded 0
+    pub rmajor: u32,
+    pub rminor: u32,
+    pub len: u32,
+}
+
+impl ArchiveMd {
+    pub fn from(state: &ArchiveState, md: &fs::Metadata) -> io::Result<ArchiveMd> {
+        let mtime: u32 = match state.props.fixed_mtime {
+            Some(t) => t,
+            None => match u32::try_from(md.mtime()) {
+                // check for 2106 epoch overflow
+                Err(_) => return Err(io::Error::new(
+                              io::ErrorKind::InvalidInput,
+                              "mtime too large for cpio",
+                          )),
+                Ok(m) => m,
+            }
+        };
+
+        let mode = md.mode();
+        let (nlink, rmajor, rminor) = match mode & S_IFMT {
+            // careful, this is confusingly not a bitwise or
+            S_IFBLK | S_IFCHR => {
+                // Linux kernel uses 32-bit dev_t, encoded as mmmM MMmm. glibc
+                // uses 64-bit MMMM Mmmm mmmM MMmm, which is compatible.
+                let rd = md.rdev();
+                (
+                 u32::try_from(md.nlink()).ok(),
+                 (((rd >> 32) & 0xfffff000) | ((rd >> 8) & 0x00000fff)) as u32,
+                 (((rd >> 12) & 0xffffff00) | (rd & 0x000000ff)) as u32,
+                )
+            },
+            S_IFREG => {
+                if md.nlink() > 1 {
+                    // For simplicity's sake, hardlinks are archived like
+                    // regular files, i.e. they're always assigned a unique
+                    // inode number and carry a corresponding data segment (if
+                    // present). Use symlinks, or if you really need hardlinks
+                    // then create them during init.
+                    eprintln!(
+                        "(nlink={}) hardlink file data may be duplicated",
+                        md.nlink()
+                    );
+                }
+                (Some(1), 0, 0)
+            },
+            _ => (u32::try_from(md.nlink()).ok(), 0, 0),
+        };
+
+        let len = match u32::try_from(md.len()) {
+            Err(_) => return Err(io::Error::new(
+                          io::ErrorKind::InvalidInput,
+                          "file too large for newc",
+                      )),
+            Ok(l) => l,
+        };
+
+        if nlink.is_none() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "nlink too large",
+            ));
+        }
+
+        Ok(ArchiveMd {
+            mode,
+            uid: match state.props.fixed_uid {
+                Some(u) => u,
+                None => md.uid(),
+            },
+            gid: match state.props.fixed_gid {
+                Some(g) => g,
+                None => md.gid(),
+            },
+            nlink: nlink.unwrap(),
+            mtime,
+            rmajor,
+            rminor,
+            len,
+        })
+    }
+}
+
 pub fn archive_path<W: Seek + Write>(
     state: &mut ArchiveState,
     path: &Path,
-    md: &fs::Metadata,
+    md: &ArchiveMd,
     mut writer: W,
 ) -> io::Result<()> {
     let mut outpath = path;
-    let mut rmajor: u32 = 0;
-    let mut rminor: u32 = 0;
 
-    let ftype = md.file_type();
-    if ftype.is_file() || ftype.is_symlink() {
+    if md.mode & S_IFMT == S_IFREG || md.mode & S_IFMT == S_IFLNK {
         return Err(io::Error::new(
             io::ErrorKind::InvalidInput,
             "archive_path does not support files or symlinks",
@@ -121,36 +222,7 @@ pub fn archive_path<W: Seek + Write>(
         return Err(io::Error::new(io::ErrorKind::InvalidInput, "path too long"));
     }
 
-    dout!("archiving {} with mode {:o}", outpath.display(), md.mode());
-
-    if md.nlink() > u32::MAX as u64 {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "nlink too large",
-        ));
-    }
-
-    let mtime: u32 = match state.props.fixed_mtime {
-        Some(t) => t,
-        None => {
-            // check for 2106 epoch overflow
-            if md.mtime() > i64::from(u32::MAX) {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    "mtime too large for cpio",
-                ));
-            }
-            md.mtime().try_into().unwrap()
-        }
-    };
-
-    // Linux kernel uses 32-bit dev_t, encoded as mmmM MMmm. glibc uses 64-bit
-    // MMMM Mmmm mmmM MMmm, which is compatible with the former.
-    if ftype.is_block_device() || ftype.is_char_device() {
-        let rd = md.rdev();
-        rmajor = (((rd >> 32) & 0xfffff000) | ((rd >> 8) & 0x00000fff)) as u32;
-        rminor = (((rd >> 12) & 0xffffff00) | (rd & 0x000000ff)) as u32;
-    }
+    dout!("archiving {} with mode {:o}", outpath.display(), md.mode);
 
     write!(
         writer,
@@ -161,22 +233,16 @@ pub fn archive_path<W: Seek + Write>(
             state.ino += 1;
             i
         },
-        mode = md.mode(),
-        uid = match state.props.fixed_uid {
-            Some(u) => u,
-            None => md.uid(),
-        },
-        gid = match state.props.fixed_gid {
-            Some(g) => g,
-            None => md.gid(),
-        },
-        nlink = md.nlink() as u32,
-        mtime = mtime,
+        mode = md.mode,
+        uid = md.uid,
+        gid = md.gid,
+        nlink = md.nlink,
+        mtime = md.mtime,
         filesize = 0,
         major = 0,
         minor = 0,
-        rmajor = rmajor,
-        rminor = rminor,
+        rmajor = md.rmajor,
+        rminor = md.rminor,
         namesize = fname.len() + 1,
         chksum = 0
     )?;
@@ -200,7 +266,7 @@ pub fn archive_path<W: Seek + Write>(
 pub fn archive_symlink<W: Seek + Write>(
     state: &mut ArchiveState,
     path: &Path,
-    md: &fs::Metadata,
+    md: &ArchiveMd,
     symlink_tgt: &Path,
     mut writer: W,
 ) -> io::Result<()> {
@@ -218,7 +284,7 @@ pub fn archive_symlink<W: Seek + Write>(
     };
     // no zero terminator for symlink target path
 
-    if !md.file_type().is_symlink() {
+    if md.mode & S_IFMT != S_IFLNK {
         return Err(io::Error::from(io::ErrorKind::InvalidInput));
     }
 
@@ -237,28 +303,7 @@ pub fn archive_symlink<W: Seek + Write>(
         return Err(io::Error::new(io::ErrorKind::InvalidInput, "path too long"));
     }
 
-    dout!("archiving {} with mode {:o}", outpath.display(), md.mode());
-
-    if md.nlink() > u32::MAX as u64 {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "nlink too large",
-        ));
-    }
-
-    let mtime: u32 = match state.props.fixed_mtime {
-        Some(t) => t,
-        None => {
-            // check for 2106 epoch overflow
-            if md.mtime() > i64::from(u32::MAX) {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    "mtime too large for cpio",
-                ));
-            }
-            md.mtime().try_into().unwrap()
-        }
-    };
+    dout!("archiving {} with mode {:o}", outpath.display(), md.mode);
 
     write!(
         writer,
@@ -269,17 +314,11 @@ pub fn archive_symlink<W: Seek + Write>(
             state.ino += 1;
             i
         },
-        mode = md.mode(),
-        uid = match state.props.fixed_uid {
-            Some(u) => u,
-            None => md.uid(),
-        },
-        gid = match state.props.fixed_gid {
-            Some(g) => g,
-            None => md.gid(),
-        },
-        nlink = md.nlink() as u32,
-        mtime = mtime,
+        mode = md.mode,
+        uid = md.uid,
+        gid = md.gid,
+        nlink = md.nlink,
+        mtime = md.mtime,
         filesize = datalen,
         major = 0,
         minor = 0,
@@ -314,14 +353,14 @@ pub fn archive_symlink<W: Seek + Write>(
 pub fn archive_file<W: Seek + Write>(
     state: &mut ArchiveState,
     path: &Path,
-    md: &fs::Metadata,
+    md: &ArchiveMd,
     in_file: &fs::File,
     mut writer: W,
 ) -> io::Result<()> {
     let mut outpath = path;
     let mut data_align_seek: u32 = 0;
 
-    if !md.file_type().is_file() {
+    if md.mode & S_IFMT != S_IFREG {
         return Err(io::Error::new(io::ErrorKind::InvalidInput, "not a file"));
     }
 
@@ -340,45 +379,9 @@ pub fn archive_file<W: Seek + Write>(
         return Err(io::Error::new(io::ErrorKind::InvalidInput, "path too long"));
     }
 
-    dout!("archiving file {} with mode {:o}", outpath.display(), md.mode());
+    dout!("archiving file {} with mode {:o}", outpath.display(), md.mode);
 
-    let mtime: u32 = match state.props.fixed_mtime {
-        Some(t) => t,
-        None => {
-            // check for 2106 epoch overflow
-            if md.mtime() > i64::from(u32::MAX) {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    "mtime too large for cpio",
-                ));
-            }
-            md.mtime().try_into().unwrap()
-        }
-    };
-
-    let datalen: u32 = {
-        if md.len() > u64::from(u32::MAX) {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "file too large for newc",
-            ));
-        }
-        md.len().try_into().unwrap()
-    };
-
-    if md.nlink() > 1 {
-        // For simplicity's sake, hardlinks are archived like regular files,
-        // i.e. they're always assigned a unique inode number and carry a
-        // corresponding data segment (if present). Use symlinks, or if you
-        // really need hardlinks then create them during init.
-        eprintln!(
-            "{}: (nlink={}) hardlink file data may be duplicated",
-            outpath.display(),
-            md.nlink()
-        );
-    }
-
-    if state.props.data_align > 0 && datalen > state.props.data_align {
+    if state.props.data_align > 0 && md.len > state.props.data_align {
         // XXX we're "bending" the newc spec a bit here to inject zeros
         // after fname to provide data segment alignment. These zeros are
         // accounted for in the namesize, but some applications may only
@@ -414,19 +417,13 @@ pub fn archive_file<W: Seek + Write>(
             state.ino += 1;
             i
         },
-        mode = md.mode(),
-        uid = match state.props.fixed_uid {
-            Some(u) => u,
-            None => md.uid(),
-        },
-        gid = match state.props.fixed_gid {
-            Some(g) => g,
-            None => md.gid(),
-        },
-        // see hardlink note above
-        nlink = 1,
-        mtime = mtime,
-        filesize = datalen,
+        mode = md.mode,
+        uid = md.uid,
+        gid = md.gid,
+        // see hardlink note in ArchiveMd
+        nlink = md.nlink,
+        mtime = md.mtime,
+        filesize = md.len,
         major = 0,
         minor = 0,
         rmajor = 0,
@@ -454,17 +451,17 @@ pub fn archive_file<W: Seek + Write>(
     state.off += seek_len as u64;
 
     // io::copy() can reflink: https://github.com/rust-lang/rust/pull/75272 \o/
-    if datalen > 0 {
+    if md.len > 0 {
         let mut reader = io::BufReader::new(in_file);
         let copied = io::copy(&mut reader, &mut writer)?;
-        if copied != u64::from(datalen) {
-            dout!("copied {}, expected {}", copied, datalen);
+        if copied != u64::from(md.len) {
+            dout!("copied {}, expected {}", copied, md.len);
             return Err(io::Error::new(
                 io::ErrorKind::UnexpectedEof,
                 "copy returned unexpected length",
             ));
         }
-        state.off += u64::from(datalen);
+        state.off += u64::from(md.len);
         let dpad_len: usize = archive_padlen(state.off, 4).try_into().unwrap();
         write!(writer, "{pad:.padlen$}", padlen = dpad_len, pad = "\0\0\0")?;
         state.off += dpad_len as u64;
