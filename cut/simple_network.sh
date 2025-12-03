1281c6d42ffe1b0396b00cd16b269c8ec2aeba6a
David Disseldorp <ddiss@suse.de>
2025-12-03 07:18:22 +0100

    cut/simple-network: convert to rapido-cut
    Mem and cpu resource interface is a bit clunky.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/simple_network.sh
+++ cut/simple_network.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 # SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
-# Copyright (C) SUSE LLC 2022, all rights reserved.
+# Copyright (C) SUSE SA 2022-2025, all rights reserved.
 
-RAPIDO_DIR="$(realpath -e ${0%/*})/.."
-. "${RAPIDO_DIR}/runtime.vars"
-
-_rt_require_dracut_args "$RAPIDO_DIR/autorun/simple_network.sh" "$@"
-_rt_require_networking
-_rt_cpu_resources_set "1"
-_rt_mem_resources_set "512M"
-
-"$DRACUT" \
-	--install "resize ps nc hostname" \
-	--modules "base" \
-	"${DRACUT_RAPIDO_ARGS[@]}" \
-	"$DRACUT_OUT" || _fail "dracut failed"
+target/release/rapido-cut \
+	--autorun "autorun/simple_network.sh $*" \
+	--install "resize ps nc hostname cat ls" \
+	--net \
+	--include "dracut.conf.d/.empty /rapido-rsc/cpu/1" \
+	--include "dracut.conf.d/.empty /rapido-rsc/mem/512M"
