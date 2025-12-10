80ff8a973b4d0d03e2c7de10c86cd360f057c533
David Disseldorp <ddiss@suse.de>
2025-12-10 04:21:23 +0100

    cut/fstests_btrfs: convert from dracut to rapido-cut
    A relatively straightforward change. Use shell globbing instead of
    passing the globs to rapido-cut, as unlike Dracut we don't support them.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/fstests_btrfs.sh
+++ cut/fstests_btrfs.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 # SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
-# Copyright (C) SUSE LLC 2018-2022, all rights reserved.
+# Copyright (C) SUSE S.A. 2018-2025, all rights reserved.
 
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
-_rt_require_dracut_args "$RAPIDO_DIR/autorun/lib/fstests.sh" \
-			"$RAPIDO_DIR/autorun/fstests_btrfs.sh" "$@"
 _rt_require_fstests
-req_inst=()
+# rapido-cut doesn't support globbing, so do it here.
+req_inst=(${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
+	${FSTESTS_SRC}/src/log-writes/* ${FSTESTS_SRC}/src/aio-dio-regress/*)
 _rt_require_btrfs_progs req_inst
 _rt_require_pam_mods req_inst "pam_rootok.so" "pam_limits.so"
 _rt_human_size_in_b "${FSTESTS_ZRAM_SIZE:-1G}" zram_bytes \
 	|| _fail "failed to calculate memory resources"
 # need enough memory for five zram devices
-_rt_mem_resources_set "$((3072 + (zram_bytes * 5 / 1048576)))M"
+mem_rsc="$((3072 + (zram_bytes * 5 / 1048576)))M"
 
-"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+PATH="target/release:${PATH}"
+rapido-cut \
+	--autorun "autorun/lib/fstests.sh autorun/fstests_btrfs.sh $*" \
+	--include "dracut.conf.d/.empty /rapido-rsc/mem/${mem_rsc}" \
+	--install "ls cat mkdir cp mv rm ln sed readlink sleep \
+		   umount findmnt dmesg uname \
+		   tail blockdev ps rmdir resize dd vim grep find df sha256sum \
 		   strace mkfs mkfs.ext4 e2fsck tune2fs shuf free ip su \
 		   which perl awk bc touch cut chmod true false unlink \
 		   mktemp getfattr setfattr chacl attr killall hexdump sync \
@@ -29,14 +35,8 @@ _rt_mem_resources_set "$((3072 + (zram_bytes * 5 / 1048576)))M"
 		   xfs_mkfile xfs_db xfs_io wipefs filefrag losetup \
 		   chgrp du fgrep pgrep tar rev kill duperemove \
 		   fsverity keyctl openssl /etc/ssl/openssl.cnf \
-		   swapon swapoff xfs_freeze fsck ${req_inst[*]} \
-		   ${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
-		   ${FSTESTS_SRC}/src/log-writes/* \
-		   ${FSTESTS_SRC}/src/aio-dio-regress/*" \
-	--include "$FSTESTS_SRC" "$FSTESTS_SRC" \
-	--add-drivers "zram lzo lzo-rle dm-snapshot dm-flakey btrfs raid6_pq \
-		       loop scsi_debug dm-log-writes xxhash_generic ext4 \
-		       virtio_blk" \
-	--modules "base" \
-	"${DRACUT_RAPIDO_ARGS[@]}" \
-	"$DRACUT_OUT" || _fail "dracut failed"
+		   swapon swapoff xfs_freeze fsck ${req_inst[*]}" \
+	--include "$FSTESTS_SRC $FSTESTS_SRC" \
+	--install-kmod "zram lzo lzo-rle dm-snapshot dm-flakey btrfs raid6_pq \
+			loop scsi_debug dm-log-writes xxhash_generic ext4 \
+			virtio_blk"
