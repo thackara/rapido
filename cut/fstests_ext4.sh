682c7852e493539587ab28cb962c8fedade975bc
David Disseldorp <ddiss@suse.de>
2026-05-05 14:48:13 +1000

    cut/fstests_ext4: convert from dracut to rapido-cut
    Include the new fstests manifest.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/fstests_ext4.sh
+++ cut/fstests_ext4.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 # SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
-# Copyright (C) SUSE LLC 2021-2022, all rights reserved.
+# Copyright (C) SUSE S.A. 2021-2026, all rights reserved.
 
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
-_rt_require_dracut_args "$RAPIDO_DIR/autorun/lib/fstests.sh" \
-			"$RAPIDO_DIR/autorun/fstests_ext4.sh" "$@"
-_rt_require_fstests
 pam_paths=()
 _rt_require_pam_mods pam_paths "pam_rootok.so" "pam_limits.so"
 _rt_human_size_in_b "${FSTESTS_ZRAM_SIZE:-1G}" zram_bytes \
 	|| _fail "failed to calculate memory resources"
 # 2x multiplier for one test and one scratch zram. +2G as buffer
-_rt_mem_resources_set "$((2048 + (zram_bytes * 2 / 1048576)))M"
+mem_rsc="$((2048 + (zram_bytes * 2 / 1048576)))M"
 
-"$DRACUT" --install "tail blockdev ps rmdir resize dd vim grep find df sha256sum \
-		   strace mkfs shuf free ip su uuidgen \
-		   which perl awk bc touch cut chmod true false unlink \
-		   mktemp getfattr setfattr chacl attr killall hexdump sync \
-		   id sort uniq date expr tac diff head dirname seq \
-		   basename tee egrep yes mkswap timeout realpath blkdiscard \
-		   fstrim fio logger dmsetup chattr lsattr cmp stat \
-		   dbench /usr/share/dbench/client.txt hostname getconf md5sum \
-		   od wc getfacl setfacl tr xargs sysctl link truncate quota \
-		   repquota setquota quotacheck quotaon pvremove vgremove \
-		   xfs_mkfile xfs_db xfs_io wipefs filefrag losetup \
-		   chgrp du fgrep pgrep tar rev kill duperemove \
-		   fsverity keyctl openssl /etc/ssl/openssl.cnf \
-		   swapon swapoff xfs_freeze fsck dump restore \
-		   debugfs dumpe2fs e2fsck e2image e4defrag fsck.ext4 \
-		   mke2fs mkfs.ext4 resize2fs tune2fs ${pam_paths[*]} \
-		   ${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
-		   ${FSTESTS_SRC}/src/log-writes/* \
-		   ${FSTESTS_SRC}/src/aio-dio-regress/*" \
-	--include "$FSTESTS_SRC" "$FSTESTS_SRC" \
-	--add-drivers "zram lzo lzo-rle dm-flakey ext4 \
-		       loop scsi_debug dm-log-writes virtio_blk" \
-	--modules "base" \
-	"${DRACUT_RAPIDO_ARGS[@]}" \
-	"$DRACUT_OUT" || _fail "dracut failed"
+printf -v req_inst_bins 'bin %s\n' "${pam_paths[@]}"
+
+PATH="target/release:${PATH}"
+rapido-cut --manifest /dev/stdin <<EOF
+file /rapido-rsc/mem/${mem_rsc}
+include manifest/fstests.fest
+
+autorun autorun/lib/fstests.sh autorun/fstests_ext4.sh $*
+
+$req_inst_bins
+bin debugfs
+bin dumpe2fs
+bin e2fsck
+bin e2image
+bin e4defrag
+bin fsck.ext4
+bin mke2fs
+bin mkfs.ext4
+bin resize2fs
+bin tune2fs
+
+try-bin dump
+try-bin restore
+try-bin uuidgen
+
+kmod ext4
+EOF
