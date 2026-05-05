bd294cc4dd3b03b2f96088d9e7e8002652fcb42a
David Disseldorp <ddiss@suse.de>
2026-05-05 14:11:06 +1000

    cut/fstests_btrfs: include the fstests manifest
    As mentioned in the manifest commit, PAM modules still need to be
    gathered in the cut script until we have support for custom path
    searches.
    The _rt_require_fstests() call can be dropped, as rapido-cut will
    fail if FSTESTS_SRC is not set in rapido.conf .
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/fstests_btrfs.sh
+++ cut/fstests_btrfs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
-# Copyright (C) SUSE S.A. 2018-2025, all rights reserved.
+# Copyright (C) SUSE S.A. 2018-2026, all rights reserved.
 
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
-_rt_require_fstests
 req_inst=()
 _rt_require_btrfs_progs req_inst
 _rt_require_pam_mods req_inst "pam_rootok.so" "pam_limits.so"
@@ -19,180 +18,16 @@ printf -v req_inst_bins 'bin %s\n' "${req_inst[@]}"
 PATH="target/release:${PATH}"
 rapido-cut --manifest /dev/stdin <<EOF
 file /rapido-rsc/mem/${mem_rsc}
+include manifest/fstests.fest
 
 autorun autorun/lib/fstests.sh autorun/fstests_btrfs.sh $*
 
-$req_inst_bins
-bin ls
-bin cat
-bin mkdir
-bin cp
-bin mv
-bin rm
-bin ln
-bin sed
-bin readlink
-bin sleep
-bin umount
-bin findmnt
-bin dmesg
-bin uname
-bin tail
-bin blockdev
-bin ps
-bin rmdir
-bin dd
-bin grep
-bin find
-bin df
-bin sha256sum
-bin strace
-bin mkfs
 bin mkfs.ext4
-bin e2fsck
-bin tune2fs
-bin shuf
-bin free
-bin ip
-bin su
-bin which
-bin perl
-bin awk
-bin bc
-bin touch
-bin cut
-bin chown
-bin chmod
-# xfstests hardcoded
-bin /bin/true
-bin false
-bin unlink
-bin mktemp
-bin getfattr
-bin setfattr
-bin chacl
-bin attr
-bin killall
-bin hexdump
-bin sync
-bin id
-bin sort
-bin uniq
-bin date
-bin expr
-bin tac
-bin diff
-bin head
-bin dirname
-bin seq
-bin basename
-bin tee
-bin egrep
-bin yes
-bin mkswap
-bin timeout
-bin realpath
-bin blkdiscard
-bin fstrim
-bin logger
-bin chattr
-bin lsattr
-bin cmp
-bin stat
-bin hostname
-bin getconf
-bin md5sum
-bin od
-bin wc
-bin getfacl
-bin setfacl
-bin tr
-bin xargs
-bin sysctl
-bin link
-bin truncate
-bin quota
-bin repquota
-bin setquota
-bin quotacheck
-bin quotaon
-bin pvremove
-bin vgremove
-bin xfs_mkfile
-bin xfs_db
-bin xfs_io
-bin wipefs
-bin filefrag
-bin losetup
-bin chgrp
-bin du
-bin fgrep
-bin pgrep
-bin tar
-bin rev
-bin kill
-bin swapon
-bin swapoff
-bin xfs_freeze
-bin fsck
-bin ipcmk
-bin ipcs
-bin ipcrm
-bin blkid
-bin mkfifo
-bin mknod
-bin flock
-bin unshare
-
-# udev needed for dm devices
-bin dmsetup
-bin udevadm
-bin systemd-udevd
-# systemd-udevd needs /run to exist for /run/udev creation
-dir /run
-# TODO: we should only need dm rules - generate on boot?
-tree /usr/lib/udev/rules.d /usr/lib/udev/rules.d
-
-# rapido-cut adds bash by default, but xfstests hardcodes /bin/bash so make
-# sure we have it there (as a symlink)
-bin /bin/bash
-# /bin/sh is also used
-slink /bin/sh /bin/bash
-# btrfs/058 calls xfs_io interactively; silence "Cannot read termcap database"
-try-bin /usr/share/terminfo/l/linux
-
-try-bin dbench
-try-bin /usr/share/dbench/client.txt
-try-bin duperemove
-try-bin fsverity
-try-bin keyctl
-try-bin openssl
-try-bin /etc/ssl/openssl.cnf
-try-bin nano
-try-bin fio
-try-bin setcap
-try-bin getcap
-try-bin capsh
-
-filter ${FSTESTS_SRC}/.git
-bin $FSTESTS_SRC
+$req_inst_bins
 
 kmod btrfs
-kmod zram
-kmod lzo
-kmod lzo_rle
 kmod raid6_pq
 kmod xxhash_generic
-
-try-kmod loop
-try-kmod dm-snapshot
-try-kmod dm-thin-pool
-try-kmod dm-zero
-try-kmod dm-delay
-try-kmod dm-flakey
-try-kmod dm-log-writes
-try-kmod scsi_debug
+# needed for tests/btrfs/012 and tests/btrfs/136
 try-kmod ext4
-# only needed if passing through devs instead of zram...
-try-kmod virtio_blk
 EOF
