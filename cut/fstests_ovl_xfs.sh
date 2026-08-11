2c132cf19243e9557bc984f23fb1e81e836fc726
David Disseldorp <ddiss@suse.de>
2026-08-11 12:10:39 +1000

    cut/fstests_ovl_xfs: convert from dracut to rapido-cut
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- cut/fstests_ovl_xfs.sh
+++ cut/fstests_ovl_xfs.sh
@@ -5,36 +5,43 @@
 RAPIDO_DIR="$(realpath -e ${0%/*})/.."
 . "${RAPIDO_DIR}/runtime.vars"
 
-_rt_require_dracut_args "$RAPIDO_DIR/autorun/lib/fstests.sh" \
-			"$RAPIDO_DIR/autorun/fstests_ovl_xfs.sh" "$@"
 req_inst=()
-_rt_require_fstests
 _rt_require_pam_mods req_inst "pam_rootok.so" "pam_limits.so"
 _rt_human_size_in_b "${FSTESTS_ZRAM_SIZE:-1G}" zram_bytes \
 	|| _fail "failed to calculate memory resources"
 # 2x multiplier for one test and one scratch zram. +2G as buffer
-_rt_mem_resources_set "$((2048 + (zram_bytes * 2 / 1048576)))M"
+mem_rsc="$((2048 + (zram_bytes * 2 / 1048576)))M"
 
-"$DRACUT" --install "tail blockdev ps rmdir stty dd vim grep find df sha256sum \
-		   strace mkfs mkfs.xfs free ip su uuidgen losetup ipcmk \
-		   which perl awk bc touch cut chmod true false unlink \
-		   mktemp getfattr setfattr chacl attr killall hexdump sync \
-		   id sort uniq date expr tac diff head dirname seq \
-		   basename tee egrep yes mkswap timeout blkdiscard \
-		   fstrim fio logger dmsetup chattr lsattr cmp stat \
-		   dbench /usr/share/dbench/client.txt hostname getconf md5sum \
-		   od wc getfacl setfacl tr xargs sysctl link truncate quota \
-		   repquota setquota quotacheck quotaon pvremove vgremove \
-		   xfs_mkfile xfs_db xfs_io xfs_spaceman fsck comm indent \
-		   xfs_mdrestore xfs_bmap xfs_fsr xfsdump xfs_freeze xfs_info \
-		   xfs_logprint xfs_repair xfs_growfs xfs_quota xfs_metadump \
-		   chgrp du fgrep pgrep tar rev kill duperemove python3 \
-		   ${req_inst[*]} ${FSTESTS_SRC}/ltp/* ${FSTESTS_SRC}/src/* \
-		   ${FSTESTS_SRC}/src/log-writes/* \
-		   ${FSTESTS_SRC}/src/aio-dio-regress/*" \
-	--include "$FSTESTS_SRC" "$FSTESTS_SRC" \
-	--add-drivers "zram nvme lzo lzo-rle dm-snapshot dm-flakey overlay xfs \
-		       loop scsi_debug dm-log-writes virtio_blk" \
-	--modules "base" \
-	"${DRACUT_RAPIDO_ARGS[@]}" \
-	"$DRACUT_OUT" || _fail "dracut failed"
+printf -v req_inst_bins 'bin %s\n' "${req_inst[@]}"
+
+PATH="target/release:${PATH}"
+rapido-cut --manifest /dev/stdin <<EOF
+file /rapido-rsc/mem/${mem_rsc}
+include manifest/fstests.fest
+
+autorun autorun/lib/fstests.sh autorun/fstests_ovl_xfs.sh $*
+
+$req_inst_bins
+bin mkfs.xfs
+bin xfs_bmap
+bin xfs_db
+bin xfs_freeze
+bin xfs_fsr
+bin xfs_growfs
+bin xfs_info
+bin xfs_io
+bin xfs_logprint
+bin xfs_mdrestore
+bin xfs_metadump
+bin xfs_mkfile
+bin xfs_quota
+bin xfs_repair
+bin xfs_spaceman
+
+try-bin xfsdump
+try-bin xfsinvutil
+try-bin xfsrestore
+
+kmod xfs
+kmod overlay
+EOF
